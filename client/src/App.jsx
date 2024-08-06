1f5775ce91a85fe387e6de49412d230ac1716f1a
anandsyadav <anandyad2004@gmail.com>
2024-08-06 17:11:58 +0530

    create navbar component and signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
 import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import Signup from './pages/Signup'
 
 function App() {
   return (
     <>
+      <Navbar />
+      <Signup />
       <div>Hello all</div>
       <Footer />
     </>
